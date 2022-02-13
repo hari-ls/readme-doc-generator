e7fd86c08cc7c715bd00b2c1a60924dfa8c3f598
hari-ls <hari@logicsync.com.au>
2022-02-13 23:22:55 +1030

    added the prompts and basic file creation

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  // Project Title (input)
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the project title:",
+  },
+  // Description (editor)
+  {
+    type: "editor",
+    name: "description",
+    message: "Provide a short description explaining the project:",
+  },
+  // Installation Instructions (editor)
+  {
+    type: "editor",
+    name: "installation",
+    message: "Installation instructions:",
+  },
+  // Usage Information (editor)
+  {
+    type: "editor",
+    name: "usage",
+    message: "Usage information:",
+  },
+  // Contribution (editor)
+  {
+    type: "editor",
+    name: "contributing",
+    message: "Contribution guidelines:",
+  },
+  // Test Instructions (editor)
+  {
+    type: "editor",
+    name: "tests",
+    message: "Test instructions:",
+  },
+  // Choose License (list)
+  {
+    type: "list",
+    name: "license",
+    message: "Select license type:",
+    choices: [
+      "Apache license 2.0",
+      "GNU General Public License v3.0",
+      "MIT",
+      "Open Software License 3.0",
+      "The Unlicense",
+    ],
+  },
+  // GitHub Username (input)
+  {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username:",
+  },
+  // Email Address (input)
+  {
+    type: "input",
+    name: "emal",
+    message: "Enter your Email address:",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  console.log(fileName, data);
+  console.log(generateMarkdown(data));
+  fs.writeFile(fileName, generateMarkdown(data), (err) => {
+    err ? console.error(err) : console.log("Success!");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile("README.md", answers);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,37 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.username} and ${data.email}
 `;
 }
 
